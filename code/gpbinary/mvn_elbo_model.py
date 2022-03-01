5440978d44853e5af5ad96a873ea39502b587113
Moses Chan <mosesyhc@u.northwestern.edu>
2022-03-01 17:31:05 -0600

    working example for elbo maximization

--- code/gpbinary/mvn_elbo_model.py
+++ code/gpbinary/mvn_elbo_model.py
@@ -1,59 +1,86 @@
 import torch
 import torch.nn as nn
+from matern_covmat import covmat
+from likelihood import negloglik_gp
+from prediction import pred_gp
 
 
 class MVN_elbo(nn.Module):
-    def __init__(self, mu, v, Phi, lsigma, psi):
+    def __init__(self, Mu, V, Lmb, lsigma2, psi, Phi, F, theta, initLmb=True):
         super().__init__()
-        self.mu = nn.Parameter(mu)
-        self.v = nn.Parameter(v)
-        self.Phi = Phi
-        self.lsigma = nn.Parameter(lsigma)
+        self.kap = Phi.shape[1]
+        if initLmb:
+            lmb = torch.Tensor(0.5 * torch.log(torch.Tensor([theta.shape[1]])) +
+                                torch.log(torch.std(theta, 0)))
+            lmb = torch.cat((lmb, torch.Tensor([0])))
+            Lmb = lmb.repeat(self.kap, 1)
+        self.Lmb = nn.Parameter(Lmb)
+        self.Mu = nn.Parameter(Mu) #, requires_grad=False)
+        self.V = nn.Parameter(V) #, requires_grad=False)
+        self.lsigma2 = nn.Parameter(lsigma2)
         self.psi = psi
+        self.Phi = Phi
+        self.F = F
+        self.theta = theta
+        self.m, self.n = F.shape
 
-    def forward(self):
-        mu = self.mu
-        v = self.v
+    def forward(self, theta0):
+        Lmb = self.Lmb
+        Mu = self.Mu
 
         psi = self.psi
         Phi = self.Phi
+        theta = self.theta
 
-        fpred = psi + (Phi @ mu).unsqueeze(1)
+        kap = self.kap
+        n0 = theta0.shape[0]
 
-        return fpred
+        ghat = torch.zeros(kap, n0)
+        for k in range(kap):
+            ghat[k], _ = pred_gp(lmb=Lmb[k], theta=theta, thetanew=theta0, g=Mu[k])
 
-    def elbo(self):
-        mu = self.mu
-        v = self.v
+        fhat = psi + Phi @ ghat
+        return fhat
+
+    def negelbo(self):
+        Lmb = self.Lmb
+        Mu = self.Mu
+        V = self.V
+        lsigma2 = self.lsigma2
+
         psi = self.psi
         Phi = self.Phi
-        lsigma = self.lsigma
+        F = self.F
+        theta = self.theta
 
-        m, kap = Phi.shape
+        m = self.m
+        n = self.n
+        kap = self.kap
 
-        s2inv = torch.exp(-2 * lsigma)
+        def predmean_gp_(Vh, lmb, theta, thetanew, g):
+            Rinv_g = Vh @ Vh.T @ g
+            R_no = covmat(thetanew, theta, lmb)
+            return R_no @ Rinv_g
 
-        Rinv =  torch.zeros((m+kap, m+kap))
-        Rinv[:kap, :kap] = torch.diag(1/v) + s2inv * Phi.T @ Phi
-        Rinv[:kap, kap:(m+kap)] = -s2inv * Phi.T
-        Rinv[kap:(m+kap), :kap] = -s2inv * Phi
-        Rinv[kap:(m+kap), kap:(m+kap)] = s2inv * torch.eye(m)
+        negelbo = -1/2 * torch.sum(torch.log(V)) + \
+            1/2 * torch.exp(-lsigma2) * torch.sum(torch.diag(Phi.T @ Phi) @ V) + \
+            m*n/2 * lsigma2
 
-        Z = torch.zeros((m+kap))
-        Z[:kap] = mu
-        Z[kap:(m+kap)] = psi.squeeze() + Phi @ mu
+        Mupred = torch.zeros((kap, n))
+        for k in range(kap):
+            negloggp_k, Vh_k = negloglik_gp(lmb=Lmb[k], theta=theta, g=Mu[k])
+            negelbo += negloggp_k
+            negelbo += 1/2 * torch.diag(Vh_k @ Vh_k.T) @ V[k]
+            Mupred[k] = predmean_gp_(Vh=Vh_k, lmb=Lmb[k], theta=theta,
+                                     thetanew=theta, g=Mu[k])
 
-        negelbo = 1/2 * Z.T @ Rinv @ Z - 1/2 * torch.sum(torch.log(v))
+        residF = F - (psi + Phi @ Mupred)
 
-        return negelbo
+        negelbo += 1/2 * torch.exp(-lsigma2) * (residF.T @ residF).sum()
+        # print(lsigma2, V.mean())
+        return negelbo.squeeze()
 
-    def test_mse(self, f0):
-        mu = self.mu
-        v = self.v
-        psi = self.psi
-        Phi = self.Phi
-        lsigma = self.lsigma
 
-        fpred = psi + (Phi @ mu).unsqueeze(1)
-
-        return ((fpred - f0) ** 2).mean()
+    def test_mse(self, theta0, f0):
+        fhat = self.forward(theta0)
+        return ((fhat - f0) ** 2).mean()
--- code/gpbinary/test_mvn_elbo.py
+++ code/gpbinary/test_mvn_elbo.py
@@ -4,11 +4,11 @@ from mvn_elbo_model import MVN_elbo
 from fayans_support import read_only_complete_data
 torch.set_default_dtype(torch.double)
 import matplotlib.pyplot as plt
-
+import numpy as np
 torch.autograd.set_detect_anomaly(True)
 
 
-nepoch_nn = 10
+nepoch_nn = 100
 nepoch_elbo = 250
 
 f, x0, theta = read_only_complete_data(r'code/data/')
@@ -22,10 +22,11 @@ m, n = f.shape  # nloc, nparam
 ntrain = 50
 ntest = 200
 
+torch.manual_seed(0)
 tempind = torch.randperm(n)
 tr_inds = tempind[:ntrain]
 te_inds = tempind[-ntest:]
-# torch.seed()
+torch.seed()
 ftr = f[:, tr_inds]
 thetatr = theta[tr_inds]
 fte = f[:, te_inds]
@@ -39,72 +40,73 @@ x = torch.column_stack((x0[:, 0], x0[:, 1],
                         *[x0[:, 2] == k for k in torch.unique(x0[:, 2])]))
 F = ftr - psi
 
-from basis_nn_model import Basis
+if False:
+    from basis_nn_model import Basis
+    def loss(class_Phi, F):
+        Phi = class_Phi()
+        if class_Phi.normalize:
+            Fhat = Phi @ Phi.T @ F
+        else:
+            Fhat = Phi @ torch.linalg.solve(Phi.T @ Phi, Phi.T) @ F
+        mse = nn.MSELoss(reduction='mean')
+        return mse(Fhat, F)
 
 
-def loss(class_Phi, F):
-    Phi = class_Phi()
-    if class_Phi.normalize:
-        Fhat = Phi @ Phi.T @ F
-    else:
-        Fhat = Phi @ torch.linalg.solve(Phi.T @ Phi, Phi.T) @ F
-    mse = nn.MSELoss(reduction='mean')
-    return mse(Fhat, F)
+    # get_bestPhi takes F and gives the SVD U
+    Phi_as_param = Basis(m, kap, normalize=True)
+    optim_nn = torch.optim.Adam(Phi_as_param.parameters(), lr=10e-3)
 
+    print('F shape:', F.shape)
 
-# get_bestPhi takes F and gives the SVD U
-Phi_as_param = Basis(m, kap, normalize=True)
-optim_nn = torch.optim.Adam(Phi_as_param.parameters(), lr=10e-3)
+    print('Neural network training:')
+    print('{:<5s} {:<12s}'.format('iter', 'train MSE'))
+    for epoch in range(nepoch_nn):
+        optim_nn.zero_grad()
+        l = loss(Phi_as_param, F)
+        l.backward()
+        optim_nn.step()
+        if (epoch % 50 - 1) == 0:
+            print('{:<5d} {:<12.6f}'.format(epoch, l))
+    Phi_match = Phi_as_param().detach()
 
-print('F shape:', F.shape)
+    print('Reproducing Phi0 error in prediction of F: ',
+          torch.mean((Phi_match @ Phi_match.T @ F - F) ** 2))
 
-print('Neural network training:')
-print('{:<5s} {:<12s}'.format('iter', 'train MSE'))
-for epoch in range(nepoch_nn):
-    optim_nn.zero_grad()
-    l = loss(Phi_as_param, ftr - psi)
-    l.backward()
-    optim_nn.step()
-    if (epoch % 50 - 1) == 0:
-        print('{:<5d} {:<12.6f}'.format(epoch, l))
-Phi_match = Phi_as_param().detach()
+    Phi = Phi_match
+    print('Basis size: ', Phi.shape)
+    np.savetxt('Phi_seed0.txt', Phi.numpy())
 
-print('Reproducing Phi0 error in prediction of F: ',
-      torch.mean((Phi_match @ Phi_match.T @ F - F) ** 2))
+Phi = torch.tensor(np.loadtxt('Phi_seed0.txt'))
 
-Phi = Phi_match
-print('Basis size: ', Phi.shape)
-
-mu = torch.zeros((kap))
-v = torch.ones((kap))
-lsigma = torch.Tensor((0,))
-model = MVN_elbo(mu=mu, v=v, Phi=Phi, lsigma=lsigma, psi=psi)
+Lmb = torch.zeros(kap, d + 1)
+Mu = Phi.T @ F
+V = torch.ones((kap, ntrain)) * 0.01
+lsigma2 = torch.Tensor([0])
+model = MVN_elbo(Mu=Mu, V=V, Lmb=Lmb,
+                 lsigma2=lsigma2, psi=torch.zeros_like(psi),
+                 Phi=Phi, F=F, theta=thetatr)
 model.double()
 
 # # .requires_grad_() turns all parameters on.
 # model.requires_grad_()
 # print(list(model.parameters()))
 
-ftrpred = model()
+ftrpred = model(thetatr)
 print('ELBO model training MSE: {:.3f}'.format(torch.mean((F - ftrpred) ** 2)))
 # optim = torch.optim.LBFGS(model.parameters(), lr=10e-2, line_search_fn='strong_wolfe')
 optim = torch.optim.AdamW(filter(lambda p: p.requires_grad, model.parameters()),
-                          lr=10e-2)  # , line_search_fn='strong_wolfe')
-
-for p in model.parameters():
-    print(p)
-
+                          lr=10e-3)  # , line_search_fn='strong_wolfe')
 header = ['iter', 'neg elbo', 'test mse', 'train mse']
 print('\nELBO training:')
 print('{:<5s} {:<12s} {:<12s} {:<12s}'.format(*header))
 for epoch in range(nepoch_elbo):
     optim.zero_grad()
-    elbo = model.elbo()
-    elbo.backward()
+    negelbo = model.negelbo()
+    negelbo.backward()
     optim.step()  # lambda: model.lik())
 
-    mse = model.test_mse(fte)
-    trainmse = model.test_mse(ftr)
-    if epoch % 25 == 0:
-        print('{:<5d} {:<12.3f} {:<12.3f} {:<12.3f}'.format(
-            epoch, elbo, mse, trainmse))
+    mse = model.test_mse(thetate, fte - psi)
+    trainmse = model.test_mse(thetatr, F)
+    # if epoch % 25 == 0:
+    print('{:<5d} {:<12.3f} {:<12.3f} {:<12.3f}'.format
+          (epoch, negelbo, mse, trainmse))
