2d64edd8cb88b2f293817cb9703456bd28fbf37f
Moses Chan <mosesyhc@u.northwestern.edu>
2022-03-08 17:48:44 -0600

    elbo with mu, V solutions and tests

--- code/gpbinary/test_surmise_Phi.py
+++ code/gpbinary/test_surmise_Phi.py
@@ -3,7 +3,7 @@ import numpy as np
 from fayans_support import read_only_complete_data
 
 
-def surmise_baseline(ntrain, ntest, run=None, seed=None):
+def surmise_baseline(ntrain, ntest, run=None, seed=None, Phi=None):
     f, x0, theta = read_only_complete_data(r'code/data/')
 
     f = torch.tensor(f)
@@ -24,10 +24,23 @@ def surmise_baseline(ntrain, ntest, run=None, seed=None):
     thetate = theta[te_inds]
 
     # SURMISE BLOCK
+    if Phi is not None:
+        offset = ftr.mean(1)
+        scale = np.ones(ftr.shape[0])
+        S = np.ones(Phi.shape[1])
+        fs = ((ftr.T - offset) / scale).T
+        extravar = torch.mean((fs - Phi @ Phi.T @ fs) ** 2, 1) * (scale ** 2)
+        standardpcinfo = {'offset': offset.numpy(),
+                          'scale': scale,
+                          'U': Phi.numpy(),
+                          'S': S,
+                          'fs': fs.T.numpy(),
+                          'extravar': extravar.numpy()}
     from surmise.emulation import emulator
     emu = emulator(x=x0.numpy(), theta=thetatr.numpy(),
                    f=ftr.numpy(), method='PCGPwM',
-                   args={'warnings': True})
+                   args={'warnings': True,
+                         'standardpcinfo': standardpcinfo})
 
     emupred = emu.predict(x=x0.numpy(), theta=thetate.numpy())
     emumse = ((emupred.mean() - fte.numpy()) ** 2).mean()
