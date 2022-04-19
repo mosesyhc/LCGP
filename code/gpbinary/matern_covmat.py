e3401cd8b1b3d95848d2d7ec6f7789f9bb3bc5af
Moses Chan <mosesyhc@u.northwestern.edu>
2022-04-19 10:35:29 -0500

    rename variables to avoid confusion
    lmb, Lmb remains to be GP hyperparameters.
    the diagonal matrix is now called Delta

--- code/gpbinary/matern_covmat.py
+++ code/gpbinary/matern_covmat.py
@@ -51,17 +51,17 @@ def cov_sp(theta, thetai, lsigma2, lmb):  # assuming x1 = x2 = theta
     C_r = c_full_i @ C_iinv @ c_full_i.T
 
     diag = torch.diag(C_full - C_r) + torch.exp(lsigma2)
-    Lmb_inv_diag = 1 / diag
+    Delta_inv_diag = 1 / diag
 
-    R = C_i + (c_full_i.T * Lmb_inv_diag) @ c_full_i  # p x p
+    R = C_i + (c_full_i.T * Delta_inv_diag) @ c_full_i  # p x p
 
     W_R, U_R = torch.linalg.eigh(R)
-    Q_half = (Lmb_inv_diag * c_full_i.T).T @ (U_R * torch.sqrt(1 / torch.abs(W_R))) @ U_R.T
+    Q_half = (Delta_inv_diag * c_full_i.T).T @ (U_R * torch.sqrt(1 / W_R)) @ U_R.T
     # Rinv_half = U_R @ torch.diag(torch.sqrt(1 / W_R)) @ U_R.T  # = Q_half = Lmb_inv @ c_full_i @ R_invhalf
 
-    # C_sp_inv = torch.diag(Lmb_inv_diag) - Q_half @ Q_half.T
+    # C_sp_inv = torch.diag(Delta_inv_diag) - Q_half @ Q_half.T
     # C_sp_inv = Lmb_inv - Lmb_inv @ c_full_i @ Rinv @ c_full_i.T @ Lmb_inv  # improve to p x p matrices
 
     logdet_C_sp = torch.log(W_R).sum() - torch.log(W_i).sum() + torch.log(diag).sum()
 
-    return Lmb_inv_diag, Q_half, logdet_C_sp
+    return Delta_inv_diag, Q_half, logdet_C_sp
