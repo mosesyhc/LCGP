0cd4e67f07fc0dce40157216a32f54e153501ff8
Moses Chan <mosesyhc@u.northwestern.edu>
2022-04-22 21:50:32 -0500

    match with sparse model. check for retain_graph issue

--- code/gpbinary/mvn_elbo_autolatent_model.py
+++ code/gpbinary/mvn_elbo_autolatent_model.py
@@ -6,27 +6,31 @@ from prediction import pred_gp
 
 
 class MVN_elbo_autolatent(nn.Module):
-    def __init__(self, Lmb, lsigma2, psi, Phi, F, theta, initLmb=True):
+    def __init__(self, Lmb, lsigma2, psi, Phi, F, theta, initLmb=True, initsigma2=True):
         super().__init__()
         self.kap = Phi.shape[1]
         self.m, self.n = F.shape
-        if initLmb:
-            lmb = torch.Tensor(0.5 * torch.log(torch.Tensor([theta.shape[1]])) +
-                                torch.log(torch.std(theta, 0)))
-            lmb = torch.cat((lmb, torch.Tensor([0])))
-            Lmb = lmb.repeat(self.kap, 1)
-        self.Lmb = nn.Parameter(Lmb)
-        self.lsigma2 = nn.Parameter(lsigma2)
         self.Mu = torch.zeros(self.kap, self.n)
         self.V = torch.zeros(self.kap, self.n)
         self.psi = psi
         self.Phi = Phi
         self.F = F
         self.theta = theta
+        if initLmb:
+            lmb = torch.Tensor(0.5 * torch.log(torch.Tensor([theta.shape[1]])) +
+                               torch.log(torch.std(theta, 0)))
+            lmb = torch.cat((lmb, torch.Tensor([0])))
+            Lmb = lmb.repeat(self.kap, 1)
+            Lmb[:, -1] = torch.log(torch.var(Phi.T @ self.F, 1))
+        self.Lmb = nn.Parameter(Lmb)
+        if initsigma2:
+            lsigma2 = nn.Parameter(torch.log(((Phi @ Phi.T @ self.F - self.F)**2).mean()))
+        self.lsigma2 = lsigma2  # nn.Parameter(torch.tensor((-8,)), requires_grad=False)
 
     def forward(self, theta0):
         Lmb = self.Lmb
         Mu = self.Mu
+        lsigma2 = self.lsigma2
 
         psi = self.psi
         Phi = self.Phi
@@ -37,7 +41,7 @@ class MVN_elbo_autolatent(nn.Module):
 
         ghat = torch.zeros(kap, n0)
         for k in range(kap):
-            ghat[k], _ = pred_gp(lmb=Lmb[k], theta=theta, thetanew=theta0, g=Mu[k])
+            ghat[k], _ = pred_gp(lmb=Lmb[k], theta=theta, thetanew=theta0, lsigma2=lsigma2, g=Mu[k])
 
         fhat = psi + Phi @ ghat
         return fhat
