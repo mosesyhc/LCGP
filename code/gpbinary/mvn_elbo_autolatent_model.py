80ec5ae8f7893522d050b33e571c34362a94aeee
Moses Chan <mosesyhc@u.northwestern.edu>
2022-04-23 09:29:11 -0500

    Same fix for full model involving M, V

--- code/gpbinary/mvn_elbo_autolatent_model.py
+++ code/gpbinary/mvn_elbo_autolatent_model.py
@@ -10,8 +10,7 @@ class MVN_elbo_autolatent(nn.Module):
         super().__init__()
         self.kap = Phi.shape[1]
         self.m, self.n = F.shape
-        self.Mu = torch.zeros(self.kap, self.n)
-        self.V = torch.zeros(self.kap, self.n)
+        self.M = torch.zeros(self.kap, self.n)
         self.psi = psi
         self.Phi = Phi
         self.F = F
@@ -24,12 +23,12 @@ class MVN_elbo_autolatent(nn.Module):
             Lmb[:, -1] = torch.log(torch.var(Phi.T @ self.F, 1))
         self.Lmb = nn.Parameter(Lmb)
         if initsigma2:
-            lsigma2 = nn.Parameter(torch.log(((Phi @ Phi.T @ self.F - self.F)**2).mean()))
-        self.lsigma2 = lsigma2  # nn.Parameter(torch.tensor((-8,)), requires_grad=False)
+            lsigma2 = torch.log(((Phi @ Phi.T @ self.F - self.F)**2).mean())
+        self.lsigma2 = nn.Parameter(lsigma2)  # nn.Parameter(torch.tensor((-8,)), requires_grad=False)
 
     def forward(self, theta0):
         Lmb = self.Lmb
-        Mu = self.Mu
+        M = self.M
         lsigma2 = self.lsigma2
 
         psi = self.psi
@@ -41,15 +40,13 @@ class MVN_elbo_autolatent(nn.Module):
 
         ghat = torch.zeros(kap, n0)
         for k in range(kap):
-            ghat[k], _ = pred_gp(lmb=Lmb[k], theta=theta, thetanew=theta0, lsigma2=lsigma2, g=Mu[k])
+            ghat[k], _ = pred_gp(lmb=Lmb[k], theta=theta, thetanew=theta0, lsigma2=lsigma2, g=M[k])
 
         fhat = psi + Phi @ ghat
         return fhat
 
     def negelbo(self):
         Lmb = self.Lmb
-        Mu = self.Mu
-        V = self.V
         lsigma2 = self.lsigma2
 
         psi = self.psi
@@ -61,31 +58,46 @@ class MVN_elbo_autolatent(nn.Module):
         n = self.n
         kap = self.kap
 
-        def predmean_gp_(Vh, lmb, theta, thetanew, g):
-            Rinv_g = Vh @ Vh.T @ g
-            R_no = covmat(thetanew, theta, lmb)
-            return R_no @ Rinv_g
+        M = torch.zeros(self.kap, self.n)
+        V = torch.zeros(self.kap, self.n)
 
         negelbo = torch.zeros(1)
         for k in range(kap):
             C_k = covmat(theta, theta, Lmb[k])
             W_k, U_k = torch.linalg.eigh(C_k)
             Winv_k = 1 / W_k
-            V[k] = 1 / (torch.exp(-lsigma2) + torch.diag(U_k @ torch.diag(Winv_k) @ U_k.T))
-            Mu[k] = torch.linalg.solve(torch.eye(n) + torch.exp(lsigma2) * U_k @ torch.diag(Winv_k) @ U_k.T, Phi[:, k] @ (F - psi))
+            Mk = torch.linalg.solve(torch.eye(n) + torch.exp(lsigma2) * U_k @ torch.diag(Winv_k) @ U_k.T, Phi[:, k] @ (F - psi))
 
-            negloggp_k, _ = negloglik_gp(lmb=Lmb[k], theta=theta, g=Mu[k].clone())
+            negloggp_k, _ = negloglik_gp(lmb=Lmb[k], theta=theta, g=Mk)
             negelbo += negloggp_k
 
-        residF = F - (psi + Phi @ Mu)
+            M[k] = Mk
+            V[k] = 1 / (torch.exp(-lsigma2) + torch.diag(U_k @ torch.diag(Winv_k) @ U_k.T))
+
+        residF = F - (psi + Phi @ M)
         negelbo += m*n/2 * lsigma2
         negelbo += 1/2 * torch.exp(-lsigma2) * (residF.T @ residF).sum()
         negelbo -= 1/2 * torch.log(V).sum()
 
-        self.Mu = Mu
-        self.V = V
         return negelbo.squeeze()
 
+    def create_MV(self):
+        kap = self.kap
+        theta = self.theta
+        lsigma2 = self.lsigma2
+        Lmb = self.Lmb
+
+        n = self.n
+        psi = self.psi
+        Phi = self.Phi
+        F = self.F
+
+        for k in range(kap):
+            C_k = covmat(theta, theta, Lmb[k])
+            W_k, U_k = torch.linalg.eigh(C_k)
+            Winv_k = 1 / W_k
+            Mk = torch.linalg.solve(torch.eye(n) + torch.exp(lsigma2) * U_k @ torch.diag(Winv_k) @ U_k.T, Phi[:, k] @ (F - psi))
+            self.M[k] = Mk
 
     def test_mse(self, theta0, f0):
         fhat = self.forward(theta0)
