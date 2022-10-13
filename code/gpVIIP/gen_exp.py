bba8cc1c88045843be99e96912eee373e76f9ad9
Moses Chan <mosesyhc@u.northwestern.edu>
2022-10-13 14:22:27 -0500

    new set of params

--- code/gpVIIP/gen_exp.py
+++ code/gpVIIP/gen_exp.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-ns = [25, 50, 100, 500, 1000, 2000]
+ns = [25, 50, 100, 500, 1000]
 methods = ['surmise', 'MVGP', 'MVIP']
-ipfracs = [1, 0.5, 0.25, 0.125]
-noiseconsts = [5]
+ipfracs = [0.5, 0.25, 0.125]
+noiseconsts = [2]
 
 combs = []
 for i in range(5):
@@ -20,4 +20,4 @@ for i in range(5):
                     combs.append(comb)
 
 combs = np.array(combs)
-np.savetxt('params.txt', combs, fmt='%s', delimiter='\t')
+np.savetxt('params.txt', X=combs, fmt='%s', delimiter='\t')
