be623c48eb1275abdf37fd6de9c51a167c72251d
Moses Chan <mosesyhc@u.northwestern.edu>
2022-11-10 14:19:24 -0600

    allow lbfgs parameters in outer function

--- code/gpVIIP/optim_elbo.py
+++ code/gpVIIP/optim_elbo.py
@@ -4,6 +4,7 @@ LS_FAIL_MAX = 5
 
 def optim_elbo_lbfgs(model,
                      maxiter=500, lr=1e-1,
+                     max_ls=15, c1=1e-4, c2=0.9,
                      gtol=1e-1,
                      verbose=False):
 
@@ -20,13 +21,16 @@ def optim_elbo_lbfgs(model,
 
     epoch = 0
     ls_fail_count = 0
+    reset_optim = 0
 
-    header = ['iter', 'grad.absmax()', 'lr', 'negelbo', 'diff.', 'test mse']
+    header = ['iter', 'grad.absmax()', 'lr', 'negelbo', 'diff.']
     if verbose:
-        print('{:<5s} {:<12s} {:<12s} {:<12s} {:<12s} {:<12s}'.format(*header))
+        print('{:<5s} {:<12s} {:<12s} {:<12s} {:<12s}'.format(*header))
     while True:
         options = {'closure': closure, 'current_loss': loss,
-                   'max_ls': 15, 'damping': True}
+                   'history_size': 4,
+                   'c1': c1, 'c2': c2,
+                   'max_ls': max_ls, 'damping': True}
         loss, grad, lr, _, _, _, _, _ = optim.step(options)
         ls_fail_count += (lr < 1e-12)
 
@@ -42,12 +46,18 @@ def optim_elbo_lbfgs(model,
                 flag = 'G_CONV'
                 break
         if ls_fail_count > LS_FAIL_MAX:
-            print('exit at epoch {:d}, line searches failed for {:d} iterations'.format(epoch, LS_FAIL_MAX))
-            flag = 'LS_FAIL_MAX_REACHED'
-            break
+            if reset_optim > 0:
+                flag = 'LS_FAIL_MAX_REACHED'
+                print('exit at epoch {:d}, line searches failed for {:d} iterations'.format(epoch, LS_FAIL_MAX))
+                break
+            else:
+                optim = torch.optim.FullBatchLBFGS(filter(lambda p: p.requires_grad, model.parameters()), lr=lr)
+                reset_optim += 1
+                ls_fail_count = 0
+                print('reset optimizer')
         if verbose:
             print('{:<5d} {:<12.3f} {:<12.3E} {:<12.3f} {:<12.3f}'.format
-                  (epoch, grad.abs().mean(), lr, loss, loss_prev - loss))
+                  (epoch, grad.abs().max(), lr, loss, loss_prev - loss))
 
         with torch.no_grad():
             loss_prev = loss.clone()
