6f04b28e472ba0adfe5e363e527b4106d5e17b40
Moses Chan <mosesyhc@u.northwestern.edu>
2022-11-15 16:21:07 -0600

    params generator (again)

---
+++ code/gpVIIP/params/make_param.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+fnames = ['borehole', 'piston', 'wingweight', 'otlcircuit']
+ns = [100, 250, 500]
+noiseconsts = [0.1]
+methods = ['MVGP', 'surmise']
+ipfracs = [1]
+
+base = np.stack(np.meshgrid(ns, methods, noiseconsts, ipfracs, fnames)).reshape(5, -1).T
+
+combs = []
+rep = 5
+for i in range(rep):
+    nseed = len(ipfracs) * len(noiseconsts) * len(ns) * len(fnames)
+    seeds = np.random.randint(0, 100000, nseed)
+
+    comb = np.insert(base, 2, np.tile(seeds, len(methods)), axis=1)
+    combs.extend(comb)
+
+np.savetxt('params_gen.txt', combs, delimiter='\t', fmt='%s')
