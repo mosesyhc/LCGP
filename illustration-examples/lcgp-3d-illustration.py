7b0da8e97b9ae233ba2dd4a315282d49b90cc647
Moses Chan <mosesyhc@u.northwestern.edu>
2023-07-27 11:08:19 -0500

    record time

--- illustration-examples/lcgp-3d-illustration.py
+++ illustration-examples/lcgp-3d-illustration.py
@@ -4,6 +4,7 @@ plt.rcParams.update({'font.size': 14,
                      'lines.markersize': 12})
 import torch
 import numpy as np
+import time
 
 from tests.functions import forrester2008
 
@@ -13,9 +14,13 @@ n = 250
 x = np.linspace(0, 1, n)
 xpred = x[1:] - 1/(2*n)
 
+# np.random.seed(0)
+
 ytrain = forrester2008(x, noisy=True, noises=[0.005, 0.1, 0.3])
 truey = forrester2008(xpred, noisy=False)
 newy = forrester2008(xpred, noisy=True, noises=[0.005, 0.1, 0.3])
+
+# np.random.seed(None)
 
 x = torch.tensor(x).unsqueeze(1)
 xpred = torch.tensor(xpred).unsqueeze(1)
@@ -25,28 +30,56 @@ ytrain = torch.tensor(ytrain)
 
 from lcgp import LCGP, evaluation
 
-lcgp = LCGP(y=ytrain, x=x, q=3, parameter_clamp=False)
+# save = {}
+# for lLmb in [0, 10, 20, 40]:
+#     for lLmb0 in [0, 5, 10, 20, 40]:
+#         pc = {'lLmb': lLmb, 'lLmb0': lLmb0}
+#         print(pc)
+
+start0 = time.monotonic()
+
+lcgp = LCGP(y=ytrain, x=x, q=3,
+            parameter_clamp=False)
 lcgp.compute_aux_predictive_quantities()
 lcgp.fit(verbose=True)
+
+end0 = time.monotonic()
 
 yhat, ypredvar, yconfvar = lcgp.predict(xpred, return_fullcov=False)
 
 print(
-evaluation.rmse(truey, yhat.numpy()),
-evaluation.intervalstats(newy, yhat.numpy(), ypredvar.numpy())
+    evaluation.rmse(truey, yhat.numpy()),
+    evaluation.normalized_rmse(truey, yhat.numpy()),
+    evaluation.intervalstats(newy, yhat.numpy(), ypredvar.numpy())
 )
 
-lcgp_r = LCGP(y=ytrain, x=x, q=3, parameter_clamp=False, robust_mean=True)
+start1 = time.monotonic()
+
+lcgp_r = LCGP(y=ytrain, x=x, q=3,
+              parameter_clamp=False, robust_mean=True)
 lcgp_r.compute_aux_predictive_quantities()
 lcgp_r.fit(verbose=True)
+
+end1 = time.monotonic()
 
 yhat_r, ypredvar_r, yconfvar_r = lcgp_r.predict(xpred, return_fullcov=False)
 
 print(
-evaluation.rmse(truey, yhat_r.numpy()),
-evaluation.intervalstats(newy, yhat_r.numpy(), ypredvar_r.numpy())
+    evaluation.rmse(truey, yhat_r.numpy()),
+    evaluation.normalized_rmse(truey, yhat_r.numpy()),
+    evaluation.intervalstats(newy, yhat_r.numpy(), ypredvar_r.numpy())
 )
+#
+# save[(lLmb, lLmb0)] = np.array((evaluation.rmse(truey, yhat.numpy()),
+#                                evaluation.normalized_rmse(truey, yhat.numpy()),
+#                                evaluation.intervalstats(newy, yhat.numpy(), ypredvar.numpy()),
+#                                evaluation.rmse(truey, yhat_r.numpy()),
+#                                evaluation.normalized_rmse(truey, yhat_r.numpy()),
+#                                evaluation.intervalstats(newy, yhat_r.numpy(), ypredvar_r.numpy())
+#                                ))
 
+
+# print(save)
 #################################################
 # svgp
 import gpytorch
@@ -54,10 +87,12 @@ from tqdm import tqdm
 num_output = 3
 num_latents = num_output
 
+
+start2 = time.monotonic()
 class MultitaskGPModel(gpytorch.models.ApproximateGP):
     def __init__(self):
         # Let's use a different set of inducing points for each latent function
-        inducing_points = torch.linspace(0, 1, int(n/4)).repeat(num_latents, 1).unsqueeze(-1)
+        inducing_points = torch.linspace(0, 1, int(n/1)).repeat(num_latents, 1).unsqueeze(-1)
 
         # We have to mark the CholeskyVariationalDistribution as batch
         # so that we learn a variational distribution for each task
@@ -119,8 +154,10 @@ for i in epochs_iter:
     loss = -mll(output, ytrain.T)
     loss.backward()
     optimizer.step()
-    if i % 100 == 0:
-        print('Loss: {:.6f}'.format(loss))
+    # if i % 100 == 0:
+    #     print('Loss: {:.6f}'.format(loss))
+
+end2 = time.monotonic()
 
 svgp.eval()
 likelihood.eval()
@@ -128,17 +165,27 @@ likelihood.eval()
 with torch.no_grad(), gpytorch.settings.fast_pred_var():
     predictions = likelihood(svgp(xpred))
     svgpmean = predictions.mean
-    svgplower, svgpupper = predictions.confidence_region()
+    svgpvar = predictions.variance
+
+
+print(
+    evaluation.rmse(truey, svgpmean.numpy().T),
+    evaluation.normalized_rmse(truey, svgpmean.numpy().T),
+    evaluation.intervalstats(newy, svgpmean.numpy().T, svgpvar.numpy().T)
+)
+
 
 
 ######################################
 #
+# # if False:
 import tensorflow as tf
 import lab as B
 from matrix import Diagonal
 from oilmm.tensorflow import OILMM
 from stheno import EQ, GP, Matern32
 
+start3 = time.monotonic()
 num_output = 3
 def build_latent_processes(ps):
     # Return models for latent processes, which are noise-contaminated GPs.
@@ -154,12 +201,15 @@ bruinsmaPrior = OILMM(tf.float64, build_latent_processes,
                       num_outputs=num_output, learn_transform=True) #np.array([1e-2]*num_output))
 bruinsmaPrior.fit(x.numpy(), ytrain.T.numpy(), trace=True)
 
+end3 = time.monotonic()
+
 bruinsmaPosterior = bruinsmaPrior.condition(x.numpy(), ytrain.T.numpy())
 bmean, bvar = bruinsmaPosterior.predict(xpred.numpy())
 
 print(
-evaluation.rmse(truey, bmean.T),
-evaluation.intervalstats(newy, bmean.T, bvar.T)
+    evaluation.rmse(truey, bmean.T),
+    evaluation.normalized_rmse(truey, bmean.T),
+    evaluation.intervalstats(newy, bmean.T, bvar.T)
 )
 
 
@@ -173,6 +223,8 @@ noises *= prior.ps.transform[0].scale()[0] ** 2
 print("Learned observation noises:", noises)
 
 ###################################################
+outputdir = 'illustration-examples/output_fig/'
+
 fig, ax = plt.subplots(1, 3, figsize=(12, 5), sharey='row')
 # for j in range(lcgp.q):
 #     ax[0].scatter(x, lcgp.g.detach()[j], marker='.', label=noise, alpha=0.5)
@@ -181,16 +233,30 @@ fig, ax = plt.subplots(1, 3, figsize=(12, 5), sharey='row')
 #     ax[0].plot(xpred, lcgp.ghat.detach()[j], label=noise, color='C{:d}'.format(j))
 # ax[0].legend(labels=['$g_1$', '$g_2$', '$g_3$'])
 
+for j in range(truey.shape[0]):
+    ax[0].plot(xpred, truey[j], label=noise, color='k', linewidth=2)
+    ax[0].set_ylabel('$f(x)$')
+    ax[0].set_xlabel('$x$')
+# ax[0].legend(labels=['$f_1$', '$f_2$', '$f_3$'])
+    ax[0].scatter(x, ytrain[j], marker='.', alpha=0.2, color='C{:d}'.format(j))
+    ax[0].plot(xpred, yhat_r.detach()[j], label=noise, color='C{:d}'.format(j))
+    # ax[1].fill_between(xpred.squeeze(), (yhat - 2*yconfvar.sqrt()).detach()[j], (yhat + 2*yconfvar.sqrt()).detach()[j], alpha=0.5, color='C{:d}'.format(j))
+    ax[0].fill_between(xpred.squeeze(), (yhat_r - 2*ypredvar_r.sqrt()).detach()[j],
+                       (yhat_r + 2*ypredvar_r.sqrt()).detach()[j], alpha=0.15, color='C{:d}'.format(j))
+    ax[0].set_ylabel(r'$\hat{f}(x)$')
+    ax[0].set_xlabel('$x$')
+    ax[0].set_title('LCGP, robust mean')
+
 for j in range(lcgp.p):
     ax[1].plot(xpred, truey[j], label=noise, color='k', linewidth=2)
     ax[1].set_ylabel('$f(x)$')
     ax[1].set_xlabel('$x$')
 # ax[1].legend(labels=['$f_1$', '$f_2$', '$f_3$'])
     ax[1].scatter(x, ytrain[j], marker='.', alpha=0.2, color='C{:d}'.format(j))
-    ax[1].plot(xpred, yhat_r.detach()[j], label=noise, color='C{:d}'.format(j))
+    ax[1].plot(xpred, yhat.detach()[j], label=noise, color='C{:d}'.format(j))
     # ax[1].fill_between(xpred.squeeze(), (yhat - 2*yconfvar.sqrt()).detach()[j], (yhat + 2*yconfvar.sqrt()).detach()[j], alpha=0.5, color='C{:d}'.format(j))
-    ax[1].fill_between(xpred.squeeze(), (yhat_r - 2*ypredvar_r.sqrt()).detach()[j],
-                       (yhat_r + 2*ypredvar_r.sqrt()).detach()[j], alpha=0.15, color='C{:d}'.format(j))
+    ax[1].fill_between(xpred.squeeze(), (yhat - 2*ypredvar.sqrt()).detach()[j],
+                       (yhat + 2*ypredvar.sqrt()).detach()[j], alpha=0.15, color='C{:d}'.format(j))
     ax[1].set_ylabel(r'$\hat{f}(x)$')
     ax[1].set_xlabel('$x$')
     ax[1].set_title('LCGP')
@@ -207,16 +273,30 @@ for j in range(truey.shape[0]):
     ax[2].set_xlabel('$x$')
     ax[2].set_title('OILMM')
 
-for j in range(truey.shape[0]):
-    ax[0].plot(xpred, truey[j], label=noise, color='k', linewidth=2)
-    ax[0].set_ylabel('$f(x)$')
-    ax[0].set_xlabel('$x$')
-# ax[0].legend(labels=['$f_1$', '$f_2$', '$f_3$'])
-    ax[0].scatter(x, ytrain[j], marker='.', alpha=0.2, color='C{:d}'.format(j))
-    ax[0].plot(xpred, svgpmean.T[j], label=noise, color='C{:d}'.format(j))
-    ax[0].fill_between(xpred.squeeze(), svgplower.T[j], svgpupper.T[j], alpha=0.15, color='C{:d}'.format(j))
-    ax[0].set_ylabel(r'$\hat{f}(x)$')
-    ax[0].set_xlabel('$x$')
-    ax[0].set_title('SVGP, VI')
 plt.tight_layout()
+plt.savefig(outputdir + 'output{:s}_{:s}.png'.format(time.strftime('%Y-%m-%d'), time.strftime('%H-%M')))
+plt.close()
 
+
+times = np.array([start0,
+                 end0,
+                 start1,
+                 end1,
+                 start2,
+                 end2,
+                 start3,
+                 end3])
+if False:
+    fig, ax = plt.subplots(3, 1, figsize=(4, 8), sharey='all')
+    for j in range(lcgp_r.q):
+        ax[j].plot(xpred, truey[j], label=noise, color='k', linewidth=2)
+        # ax[j].legend(labels=['$f_1$', '$f_2$', '$f_3$'])
+        ax[j].scatter(x, ytrain[j], marker='.', alpha=0.2, color='C{:d}'.format(j))
+        ax[j].plot(xpred, yhat_r.detach()[j], label=noise, color='C{:d}'.format(j))
+        # ax[j][1].fill_between(xpred.squeeze(), (yhat - 2*yconfvar.sqrt()).detach()[j], (yhat + 2*yconfvar.sqrt()).detach()[j], alpha=0.5, color='C{:d}'.format(j))
+        ax[j].fill_between(xpred.squeeze(), (yhat_r - 2*ypredvar_r.sqrt()).detach()[j],
+                           (yhat_r + 2*ypredvar_r.sqrt()).detach()[j], alpha=0.15, color='C{:d}'.format(j))
+        ax[j].set_ylabel(r'$y_{:d}(x)$'.format(j+1))
+        ax[j].set_xlabel('$x$')
+    plt.tight_layout()
+    plt.savefig(outputdir + 'lcgp_illustration.png', dpi=150)
