c6e5184aa3adee660eb5ac306b1e096fbf2a8370
Moses Chan <mosesyhc@u.northwestern.edu>
2023-08-03 09:24:36 -0500

    setup update

--- setup.py
+++ setup.py
@@ -1,19 +1,6 @@
-import setuptools
+from setuptools import setup, find_packages
 
-requirements=[
-    'torch>=2.0.1',
-    'numpy>=1.18.3',
-    'scipy>=1.10.1'
-]
-
-setuptools.setup(
-    packages=setuptools.find_packages(exclude=['tests']),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.8',
-    install_requires=requirements,
-    include_package_data=True
+setup(
+    name='lcgp',
+    packages=find_packages(exclude=['tests/*'])
 )
