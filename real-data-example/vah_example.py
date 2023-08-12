5f6032620e08e8474fa3bd4c2cc81e8a499f30f6
Moses Chan <mosesyhc@u.northwestern.edu>
2023-08-12 08:33:19 -0500

    update to include group

--- real-data-example/vah_example.py
+++ real-data-example/vah_example.py
@@ -10,6 +10,26 @@ datadir = 'real-data-example/data/'
 
 pathlib.Path(outputdir).mkdir(exist_ok=True)
 
+with open(datadir + '/xlabel.txt', 'r') as f:
+    xlabel = f.readlines()
+f.close()
+
+xlabel_groups = {}
+xlabel_group_counts = {}
+
+for item in xlabel:
+    item = item.strip('\n')
+    category = item.split('_')[0].strip()
+    if not category == 'pT':
+        if category in xlabel_groups:
+            xlabel_groups[category].append(item)
+            xlabel_group_counts[category] += 1
+        else:
+            xlabel_groups[category] = [item]
+            xlabel_group_counts[category] = 1
+
+err_struct = list(xlabel_group_counts.values())
+
 xtrain = np.loadtxt(datadir + '/xtrain.txt')
 ytrain = np.loadtxt(datadir + '/ytrain.txt')
 xtest = np.loadtxt(datadir + '/xtest.txt')
@@ -25,7 +45,7 @@ data = {
 robusts = [False, None, None]
 for model in [LCGPRun, SVGPRun, OILMMRun]:
     modelrun = model(runno='', data=data,
-                     num_latent=25, robust=robusts[0], )
+                     num_latent=30, robust=robusts[0], err_struct=err_struct)
     modelrun.define_model()
     traintime0 = time.time()
     modelrun.train()
