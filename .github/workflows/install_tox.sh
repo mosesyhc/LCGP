62fd1a6c11b0617546cc7e45bddc12c6d27274c5
Moses Chan <moses.chan@northwestern.edu>
2025-08-13 05:50:45 -0500

    include ruff in installing dependencies

--- .github/workflows/install_tox.sh
+++ .github/workflows/install_tox.sh
@@ -4,13 +4,16 @@ python -m pip install --upgrade pip
 python -m pip install --upgrade setuptools
 python -m pip install --upgrade wheel
 python -m pip install --upgrade tox
+python -m pip install --upgrade ruff
 echo
 which python
 which pip
 which tox
+which ruff
 echo
 python --version
 pip --version
 tox --version
+ruff --version
 echo
 pip list
