6f39d8ffafa7d24897b66db9d9a64521a1b5ff5b
Moses Chan <moses.chan@northwestern.edu>
2025-08-13 04:51:02 -0500

    specify coverage configurations and turn on tensorflow eager to make sure coverage assesses the actual code

--- lcgp/tests/test_cov.py
+++ lcgp/tests/test_cov.py
@@ -3,7 +3,6 @@ import numpy as np
 import copy
 
 from lcgp import LCGP
-from lcgp.evaluation import dss, rmse, normalized_rmse, intervalstats
 from lcgp.covmat import Matern32
 
 
@@ -89,7 +88,7 @@ class TestInit:
         model.tx_x(model.x)
         model.tx_y(model.y)
 
-@pytest.mark.parametrize('llmb, llmb0, lnug', [((0.), (0.), (-12.))])
+@pytest.mark.parametrize('llmb, llmb0, lnug', [((1.), (1.), (-12.))])
 class TestCov1D:
     @pytest.mark.parametrize('x1', [np.reshape((np.linspace(0, 1, 40)), (40, 1))])
     @pytest.mark.parametrize('x2', [np.reshape(np.linspace(0, 1, 40), (40, 1)),
@@ -121,7 +120,7 @@ class TestCov1D:
     #     with pytest.raises(AssertionError):
     #         Matern32_sp(x=x, xi=xi, llmb=llmb, llmb0=llmb0, lnug=lnug)
 
-@pytest.mark.parametrize('llmb, llmb0, lnug', [(([0., 0.]), (0.),
+@pytest.mark.parametrize('llmb, llmb0, lnug', [(([1., 1.]), (1.),
                                                 (-12.))])
 class TestCovHD:
     @pytest.mark.parametrize('X1, X2', [(np.random.randn(40, 2),
@@ -133,88 +132,3 @@ class TestCovHD:
     def test_Matern2D_diag(self, X1, llmb, llmb0, lnug):
         Matern32(x1=X1, x2=X1, llmb=llmb, llmb0=llmb0, lnug=lnug, diag_only=True)
 
-
-@pytest.mark.parametrize('y1', [np.random.normal(0, 1, (3, 40))])
-class TestEvaluation:
-    def test_rmse(self, y1):
-        y2 = y1 + 1e-16 * np.random.randn(*y1.shape)
-        assert np.isclose(rmse(y1, y2), 0.)
-
-    def test_normalized_rmse(self, y1):
-        y2 = y1 + 1e-16 * np.random.randn(*y1.shape)
-        assert np.isclose(normalized_rmse(y1, y2), 0.)
-
-    def test_dss(self, y1):
-        mean = np.zeros_like(y1)
-        p, n = y1.shape
-        cov = np.repeat(np.eye(p), n).reshape((p, p, n))
-        dss(y=y1, ypredmean=mean, ypredcov=cov, use_diag=False)
-
-    def test_dss_diag(self, y1):
-        mean = np.zeros_like(y1)
-        var = np.ones_like(y1)
-        dss(y=y1, ypredmean=mean, ypredcov=var, use_diag=True)
-
-    def test_intervals(self, y1):
-        mean = np.zeros_like(y1)
-        var = np.ones_like(y1)
-        coverage, length = intervalstats(y1, mean, var)
-        assert 0 <= coverage <= 1
-        assert length >= 0
-
-
-class TestTraining:
-    @pytest.mark.parametrize('submethod', ['full'])  #, 'elbo', 'proflik'])
-    def test_fit(self, submethod):
-        x = np.linspace(0, 1, 40)
-        y = np.reshape(copy.copy(x), (1, 40))
-        model = LCGP(y=y, x=x, submethod=submethod)
-        model.fit()
-        model.predict(x0=x)
-        # model.predict(x0=x, return_fullcov=True)
-        # model.forward(x0=x)
-        model.get_param()
-        # model.get_param_grad()
-
-    # def test_optim_edge_lr(self):
-    #     x = np.linspace(0, 1, 40)
-    #     y = copy.copy(x)
-    #     model = LCGP(y=y, x=x)
-    #     model.fit(maxiter=15, lr=20, verbose=True)
-    # 
-    # def test_optim_edge_ftol(self):
-    #     x = np.linspace(0, 1, 40)
-    #     y = copy.copy(x)
-    #     model = LCGP(y=y, x=x)
-    #     model.fit(maxiter=15, ftol=1, verbose=True)
-
-    # def test_optim_edge_pgtol(self):
-    #     x = np.linspace(0, 1, 40)
-    #     y = copy.copy(x)
-    #     model = LCGP(y=y, x=x)
-    #     model.fit(maxiter=15, pgtol=1, verbose=True)
-
-    def test_invalid_submethod(self):
-        x = np.linspace(0, 1, 40)
-        y = np.reshape(copy.copy(x), (1, 40))
-        model = LCGP(y=y, x=x, submethod='null')
-        with pytest.raises(ValueError):
-            model.fit()
-            model.predict(x0=x)
-# 
-# 
-# class TestParameterClamping:
-#     @pytest.mark.parametrize('clamping', [True, False])
-#     def test_parameter_clamping_1d(self, clamping):
-#         x = np.linspace(0, 1, 40)
-#         y = copy.copy(x)
-# 
-#         model = LCGP(y=y, x=x, parameter_clamp_flag=clamping)
-#         model.get_param()
-# 
-#     @pytest.mark.parametrize('clamping', [True, False])
-#     def test_parameter_clamping_hd(self, clamping):
-#         t = np.random.randn(4)
-#         trng = tensor([(0, 1), (0, 1), (0, 1), (0, 1)])
-# 
-#         parameter_clamping(t=t, trng=trng)
