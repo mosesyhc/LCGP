fix phi calculation in init_phi

lcgp/lcgp.py
@@ -305,7 +305,8 @@ class LCGP(gpflow.Module):
         assert left_u.shape[1] == min(n, p)
 
         sing_q = singvals[:q]
-        phi = left_u[:, :q] * tf.sqrt(tf.cast(n, tf.float64)) / sing_q
+        # phi = left_u[:, :q] * tf.sqrt(tf.cast(n, tf.float64)) / sing_q
+        phi = left_u[:, :q]
         diag_D = tf.reduce_sum(phi ** 2, axis=0)
 
         g = tf.matmul(phi, y, transpose_a=True)