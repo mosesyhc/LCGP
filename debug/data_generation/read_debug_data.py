5b015a79153a3ded3cbae0faaa19ff174b47cee1
Edbert Handjaja <edberthandjaja@gmail.com>
2026-02-16 12:04:14 -0600

    add plotting for debug

--- debug/data_generation/read_debug_data.py
+++ debug/data_generation/read_debug_data.py
@@ -4,13 +4,11 @@ def load_train_test_csv(train_csv: str, test_csv: str):
     train = np.genfromtxt(train_csv, delimiter=",", names=True)
     test  = np.genfromtxt(test_csv,  delimiter=",", names=True)
 
-    # train: columns x,y1,y2,y3
-    xtrain = train["x"].reshape(-1, 1)               # (N,1)
-    ytrain = np.vstack([train["y1"], train["y2"], train["y3"]])   # (p,N)
+    xtrain = train["x"].reshape(-1, 1)               
+    ytrain = np.vstack([train["y1"], train["y2"], train["y3"]]) 
 
-    # test: columns x,y1_true,y2_true,y3_true
     xtest = test["x"].reshape(-1, 1)
-    ytrue = np.vstack([test["y1_true"], test["y2_true"], test["y3_true"]])  # (p,Ntest)
+    ytrue = np.vstack([test["y1_true"], test["y2_true"], test["y3_true"]])  
 
     return {
         "xtrain": xtrain,
